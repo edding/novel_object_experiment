97ce5aee909a3d6c55871bbccbd24a20f5b763ce
Edward Ding <nsobject.ding.jiarui@gmail.com>
2023-04-15 15:11:14 -0500

    Style the websites with materialize

--- routes/survey.js
+++ routes/survey.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var router = express.Router();
 
+var path = require("path");
+
+/* Point to the public folder. */
+router.use(express.static("public"));
+
 router.get("/", function (req, res, next) {
   res.render("survey");
 });
