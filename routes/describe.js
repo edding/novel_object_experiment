1f92ff0291d1b07806439f6ef871bca01fd13d27
Edward Ding <nsobject.ding.jiarui@gmail.com>
2023-04-14 21:11:24 -0500

    completed the describe task, updated logging logic

--- routes/describe.js
+++ routes/describe.js
@@ -5,9 +5,19 @@ var path = require("path");
 /* Point to the public folder. */
 router.use(express.static("public"));
 
-/* GET trial page. */
 router.get("/intro", function (req, res, next) {
   res.render("describe_intro");
+});
+
+router.get("/example", function (req, res, next) {
+  res.render("describe", { id: 0, isExample: true });
+});
+
+router.get("/:id", function (req, res, next) {
+  if (req.params.id > 30) {
+    res.redirect("/completed");
+  }
+  res.render("describe", { id: req.params.id, isExample: false });
 });
 
 module.exports = router;
--- routes/log.js
+++ routes/log.js
@@ -1,27 +1,55 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-const fs = require('fs');
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+const fs = require("fs");
 
+const bodyParser = require("body-parser");
+router.use(bodyParser.urlencoded({ extended: true }));
 
 /* POST log listing. */
-router.post('/', function (req, res, next) {
-    const data = req.body.data;
+router.post("/", function (req, res, next) {
+  const uid = req.session.uid;
+  const data = req.body.data;
 
-    if (!fs.existsSync('logs')) {
-        fs.mkdirSync('logs');
-    }
+  if (!fs.existsSync("logs")) {
+    fs.mkdirSync("logs");
+  }
+  csvWriter = createCsvWriter({
+    path: "logs" + uid + ".csv",
+    header: [
+      { id: "page", title: "page" },
+      { id: "img_id", title: "img_id" },
+      { id: "x", title: "x" },
+      { id: "y", title: "y" },
+    ],
+    append: true,
+  });
+  csvWriter.writeRecords(data); // returns a promise
+});
+
+router.post("/describe/:id", function (req, res, next) {
+  const id = parseInt(req.params.id);
+  if (id > 0) {
+    // 0 represents the example, there is nothing to log
+    const uid = req.session.uid;
     csvWriter = createCsvWriter({
-        path: 'logs/student1' + '.csv',
-        header: [
-            { id: 'page', title: 'page' },
-            { id: 'img_id', title: 'img_id' },
-            { id: 'x', title: 'x' },
-            { id: 'y', title: 'y' },
-        ],
-        append: true
+      path: "logs/describe.csv",
+      header: ["uid", "object_id", "seen", "description"],
+      append: true,
     });
-    csvWriter.writeRecords(data)       // returns a promise
+
+    const records = [
+      {
+        uid: uid,
+        object_id: id,
+        seen: req.body.seen,
+        description: req.body.description,
+      },
+    ];
+    csvWriter.writeRecords(records);
+  }
+
+  res.redirect("/describe/" + (id + 1));
 });
 
 module.exports = router;
