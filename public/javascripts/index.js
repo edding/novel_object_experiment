8dc490d1dae40e8e5f692c6624463b8c5ff846cd
Edward Ding <nsobject.ding.jiarui@gmail.com>
2023-04-15 22:15:28 -0500

    Update the input validation for uid

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,10 +1,7 @@
-// TODO: Better to validate on submit and prompt error instead of validate on input
-function validateInput() {
-  const input = document.getElementById("uid_input");
-  const submitButton = document.getElementById("uid_submit");
-  if (input.value.length != 9) {
-    submitButton.disabled = true;
-  } else {
-    submitButton.disabled = false;
+function validateInput(e) {
+  const uidInput = document.getElementById("uid_input");
+  if (uidInput.value.length != 9) {
+    alert("Please enter a valid UID.");
+    e.preventDefault();
   }
 }
